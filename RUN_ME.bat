b92c62415d22792f3fc7e4384acf099bf27b433d
Hanra <37299738+HenraL@users.noreply.github.com>
2023-06-22 01:12:40 +0200

    [FIX] Tailwind deployement

--- RUN_ME.bat
+++ RUN_ME.bat
@@ -7,13 +7,10 @@ echo Upgrading pip &^
 python -m pip install --upgrade pip &^
 echo Installing requirements &^
 pip install -r files\requirements.txt &^
-deactivate
-echo All done
-echo Activating environement
-wenv\Scripts\activate &^
+echo All done &^
 echo Running script &^
 cd files &^
-python3 launch_local.py &^
+python launch_local.py &^
 cd .. &^
 echo Deactivating environement &^
 deactivate
