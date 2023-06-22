5a866b330f4dcb212344772943eda081c4a9c5a0
HenraL <HenraL@users.noreply.github.com>
2023-06-22 02:24:25 +0000

    deploy: 2a20f5a06c16ab90c561ccf86c2d1954dfe03ef5

---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+function my_test() {
+    document.getElementById("input_test").innerHTML += "<p>This is a test</p>";
+}
---
+++ static/vendor/bootstrap/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [],
+    darkMode: true, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
