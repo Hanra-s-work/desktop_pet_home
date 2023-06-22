e2c66f5bc9e67e21e2a20e3a01acd9bb566daad9
Hanra <37299738+HenraL@users.noreply.github.com>
2023-06-22 03:49:57 +0200

    [FIX] page displaying (probably)

--- files/build.py
+++ files/build.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
 import os
-print(f"os.listdir = {os.listdir()}")
-print(f"cwd = {os.getcwd()}")
+import sys
 
 app = Flask(__name__)
 
+
+def create_paths_if_not_exist(path: str = f"{os.getcwd()}/output") -> None:
+    """ Create the output path if it does not exist """
+    if os.path.exists(path) == False:
+        os.makedirs(path, exist_ok=True)
+
+
 if __name__ == '__main__':
+    print(f"cwd = {os.getcwd()}")
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     with app.app_context():
         rendered_content = render_template('index.jinja')
-
+        src_path = os.path.join(os.getcwd(), "output")
+        create_paths_if_not_exist(src_path)
         # Save the rendered content to an HTML file
-        output_file = os.path.join(os.getcwd(), 'index.html')
+        output_file = os.path.join(src_path, 'index.html')
+        print(f"output_file = {output_file}")
+        sys.stdout.flush()
         with open(output_file, 'w', encoding="utf-8") as file:
             file.write(rendered_content)
+    print(f"os.listdir = {os.listdir()}")
